7738e730394f63fbbcb03bceefbad95a5224bd4b
plebg <plebeiusgaragicus@gmail.com>
2023-03-06 10:59:16 -0800

    g

---
+++ git set-url.md
@@ -0,0 +1,9 @@
+```sh
+git remote set-url origin <new-origin-url>
+
+git remote -v
+# origin  https://github.com/user/repo.git (fetch)
+# origin  https://github.com/user/repo.git (push)
+
+git push -u origin master
+```
--- src/app.js
+++ src/app.js
@@ -2,19 +2,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 
-const session = require('express-session');
+// const redis = require('redis');
+// const session = require('express-session');
+
+// const RedisStore = require('connect-redis')(session);
+
+// const sessionStore = new RedisStore({ client: redisClient });
+
+
+// const redisClient = redis.createClient({
+//   host: 'localhost',
+//   port: 6379,
+//   password: 'your-redis-password'
+// });
+
+import connectRedis from 'connect-redis';
+import session from 'express-session';
+const RedisStore = connectRedis(session);
+
+ app.use(
+        session({
+            secret: 'secret',
+            store: new RedisStore({host: '127.0.0.1', port: 6379}),
+            resave: false,
+            saveUninitialized: false
+        })
+    );
+
+
+redisClient.on('error', (err) => {
+  console.error('Redis error:', err);
+});
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.use(session({
-  secret: 'mysecret',
-  resave: false,
-  saveUninitialized: false
-}));
 
+// app.use(session({
+//   secret: 'mysecret',
+//   resave: false,
+//   saveUninitialized: false
+// }));
+app.use(session({
+  store: sessionStore,
+  secret: 'your-session-secret',
+  resave: false,
+  saveUninitialized: true
+}));
 
 
 function performCalculation(num1, num2, operation) {
@@ -48,25 +85,14 @@ app.post('/', (req, res) => {
 });
 
 
+
 app.get('/', (req, res) => {
-  // if (!req.session.num1) {
-  //   req.session.num1 = 1;
-  // }
-  // if (!req.session.num2) {
-  //   req.session.num2 = 2;
-  // }
-  // if (!req.session.operation) {
-  //   req.session.operation = '+';
-  // }
-  // const num1 = Number(req.session.num1);
-  // const num2 = Number(req.session.num2);
   const num1 = req.session.num1 || 1;
   const num2 = req.session.num2 || 2.5;
   const operation = req.session.operation || '+';
 
   res.render('index', { num1, num2, operation });
 });
-
 
 
 
